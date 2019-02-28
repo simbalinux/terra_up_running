0a64a15bf2d4f8d43eb9219fffebb86ec945b1a1
Your Name <you@example.com>
2019-02-28 22:31:49 +0000

    added new region and AMI

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {  
   ami           = "${lookup(var.amis, var.region)}"
-  instance_type = "t1.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
   user_data = <<-EOF
     #!/bin/bash
--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {
-  default = "us-east-1"
+  default = "us-west-2"
 }
 variable "amis" {
   type = "map"
   default = {
-    "us-east-1" = "ami-b374d5a5"
+    "us-west-2" = "ami-07c2e617785343806"
   }
 }
 
